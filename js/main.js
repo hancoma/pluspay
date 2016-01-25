c78fc45fbbffc0abed22d413daa31fefae9ca13c
unknown <hancoma>
2016-01-26 02:30:48 +0900

    신규

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){ 
+		display_main();
+	});
+
+// 홈 클릭
+$("#home_btn").click(
+	function() {
+			alert("확인");
+	}
+	
+);
---
+++ js/map.js
@@ -0,0 +1,10 @@
+function display_main () {
+		var container = document.getElementById('map');
+		var options = {
+			center: new daum.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+
+		var map = new daum.maps.Map(container, options);
+	
+}
